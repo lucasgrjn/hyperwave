badf6514bce02be6ae892b6f244592714b26e75a
“jlu-spins” <jesselu@spinsphotonics.com>
2024-06-11 17:22:34 -0700

    midway through preemptive cleanup refactor

--- hyperwave/grids.py
+++ hyperwave/grids.py
@@ -1,5 +1,16 @@
+"""Definition of and operations on the Yee cell simulation volume.
+
+NOTE: We use a definition of the Yee cell simulation grid (see 
+https://en.wikipedia.org/wiki/Finite-difference_time-domain_method) where the
+{xyz}-component of the electric field is shifted by a half-cell in the {xyz}
+direction as well as along the time axis. The corresponding magnetic field
+components are shifted only along the spatial axes perpendicular to the
+component axis.
+
+"""
+
 from functools import partial
-from typing import NamedTuple, Tuple
+from typing import Tuple
 
 import jax
 import jax.numpy as jnp
@@ -7,24 +18,24 @@ from jax.typing import ArrayLike
 
 from . import defs
 
-# Alias for grid.
+# Minimal, sufficient definition of the Yee lattice for the simulation volume.
+#
+# Defines the distance between adjacent components along x-, y-, and z-axes.
+# Each of the three arrays should be of shape ``(uu, 2)`` for each of the 3
+# spatial axes, where the ``[:, 1]`` values are shifted by half a cell in the
+# positive direction of the axis in relation to the ``[:, 0]`` values.
+#
 Grid = Tuple[ArrayLike, ArrayLike, ArrayLike]
 
 
 def shape(grid: Grid) -> defs.Int3:
+    """``(xx, yy, zz)`` shape of the simulation volume according to ``grid``."""
     return tuple(du.shape[0] for du in grid)
 
 
 def is_valid(grid: Grid) -> bool:
+    """``True`` iff ``grid`` is valid."""
     return all(du.shape[1] == 2 and du.ndim == 2 for du in grid)
-
-
-def expanded_deltas(grid: Grid, axis: int, is_forward: bool) -> jax.Array:
-    index = 1 if is_forward else 0  # TODO: Not sure if this is correct.
-    return jnp.expand_dims(
-        grid[axis][:, index],
-        axis=range(axis + 1, 0) if axis < 0 else range(axis + 1, 3),
-    )
 
 
 def spatial_diff(
@@ -33,7 +44,7 @@ def spatial_diff(
     axis: int,
     is_forward: bool,
 ) -> jax.Array:
-    """Returns the spatial differences of ``field`` along ``axis``."""
+    """Spatial differences of ``field`` along ``axis``."""
     if is_forward:
         return (jnp.roll(field, shift=+1, axis=axis) - field) / delta
     else:
@@ -41,15 +52,21 @@ def spatial_diff(
 
 
 def curl(field: ArrayLike, grid: Grid, is_forward: bool) -> jax.Array:
-    """Returns the curl of ``field`` on ``grid``."""
+    """Curl of ``field`` on ``grid`` with ``is_forward=False`` for E-field."""
+
+    delta_index = 1 if is_forward else 0
+    deltas = tuple(
+        jnp.expand_dims(grid[a][:, delta_index], range(1, 3 - a)) for a in range(3)
+    )
+
     fx, fy, fz = [field[..., i, :, :, :] for i in range(3)]
     dx, dy, dz = [
         partial(
             spatial_diff,
-            delta=expanded_deltas(grid, axis=a, is_forward=is_forward),
-            axis=a,
+            delta=deltas[axis],
+            axis=axis,
             is_forward=is_forward,
         )
-        for a in range(-3, 0)
+        for axis in range(-3, 0)
     ]
     return jnp.stack([dy(fz) - dz(fy), dz(fx) - dx(fz), dx(fy) - dy(fx)], axis=-4)
